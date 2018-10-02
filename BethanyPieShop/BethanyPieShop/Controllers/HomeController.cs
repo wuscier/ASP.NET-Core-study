8a774ac2be48dac88fc75d809a76fed3e5664c67
wuscier <1352288123@qq.com>
2018-10-02 21:06:39 +0800

    add ef core

---
+++ BethanyPieShop/BethanyPieShop/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.Controllers
+{
+    public class HomeController: Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany's Pie Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ BethanyPieShop/BethanyPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BethanyPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
