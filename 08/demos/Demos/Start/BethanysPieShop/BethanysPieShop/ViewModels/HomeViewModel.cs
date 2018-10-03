c1f76369d7106b470d025015edf56f03c5a67720
wuscier <1352288123@qq.com>
2018-10-03 10:51:54 +0800

    build an enterprise app with aspnet core mvc

---
+++ 08/demos/Demos/Start/BethanysPieShop/BethanysPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Pie> Pies { get; set; }
+        public string Title { get; set; }
+
+    }
+}
