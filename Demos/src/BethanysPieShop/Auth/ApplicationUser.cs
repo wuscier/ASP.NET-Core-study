e386c088684990027c234fafd34d9f652b4a037c
wuscier <1352288123@qq.com>
2018-10-08 21:38:52 +0800

    add razor page app

---
+++ Demos/src/BethanysPieShop/Auth/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BethanysPieShop.Auth
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime Birthdate { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
